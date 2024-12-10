7624c07f6d148f13f6629de9012d77f99fc818bf
Mayan <may.santana197@gmail.com>
2024-12-10 16:31:59 -0300

    Adicionando front end

--- back-end/ReserveAqui.Application/obj/Debug/net8.0/ReserveAqui.Application.AssemblyInfo.cs
+++ back-end/ReserveAqui.Application/obj/Debug/net8.0/ReserveAqui.Application.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReserveAqui.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26848515c97f5343736cfff2adf1d633de8cb00a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a80f56fcf6a69351bec57509d20073cbde75d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReserveAqui.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReserveAqui.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- back-end/ReserveAqui.Infra/obj/Debug/net8.0/ReserveAqui.Infra.AssemblyInfo.cs
+++ back-end/ReserveAqui.Infra/obj/Debug/net8.0/ReserveAqui.Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReserveAqui.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2a76609c9c5782e2f7fe491322751a4465cc24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a80f56fcf6a69351bec57509d20073cbde75d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReserveAqui.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReserveAqui.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
